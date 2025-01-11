72f45d814a89a4487a9a3d7c21d9cd69f4f7c6df
Jaspreet Dhami <jdhami1@ualberta.ca>
2025-01-10 23:54:10 -0700

    Provides the link which I have to add to the portfolio button.

--- resume_updating_script.py
+++ resume_updating_script.py
@@ -4,38 +4,51 @@ from pydrive.drive import GoogleDrive
 def authenticate():
     gauth = GoogleAuth()
     gauth.LocalWebserverAuth()
-    return gauth
+    drive_obj = GoogleDrive(gauth)
+    return drive_obj
 
-drive = GoogleDrive(authenticate())
+def deleting_file(file_name, drive):
+    file_list = drive.ListFile({'q': f"title = '{file_name}' and trashed=false"}).GetList()
 
-# Deleting the old Resume from my Drive
-file_name = "Resume_Jaspreet_Singh_Dhami.pdf"  # Change this to your file name
-file_list = drive.ListFile({'q': f"title = '{file_name}' and trashed=false"}).GetList()
+    if len(file_list) != 0:
+    # if the file exists already, delete the file
+        old_file_id = file_list[0]['id']
+        old_file = drive.CreateFile({'id': old_file_id})
+        old_file.Delete()
 
-if len(file_list) != 0:
-# if the file exists already, delete the file
-    old_file_id = file_list[0]['id']
-    old_file = drive.CreateFile({'id': old_file_id})
-    old_file.Delete()
+def uploading_file(resume_path, file_name, drive):
+    # Creating instance
+    upload_resume = drive.CreateFile({'title': f'{file_name}'})  # Set the desired title for the uploaded file
 
-# Upload the new Resume to my Drive
-resume_path = f"../Final_Job_Search/{file_name}"
+    # Set the content of the file from given path
+    upload_resume.SetContentFile(resume_path)
 
-# Creating instance
-upload_resume = drive.CreateFile({'title': f'{file_name}'})  # Set the desired title for the uploaded file
+    # Upload the file
+    upload_resume.Upload()
 
-# Set the content of the file from given path
-upload_resume.SetContentFile(resume_path)
+    # updating the file permissions
+    upload_resume.InsertPermission({
+        'type': 'anyone',  # "anyone" means anyone with the link
+        'value': 'anyoneWithLink',  # Share with anyone who has the link
+        'role': 'reader'  # Set the role to "reader" (can view)
+    })
 
-# Upload the file
-upload_resume.Upload()
+    return upload_resume
 
-# updating the file permissions
-upload_resume.InsertPermission({
-    'type': 'anyone',  # "anyone" means anyone with the link
-    'value': 'anyoneWithLink',  # Share with anyone who has the link
-    'role': 'reader'  # Set the role to "reader" (can view)
-})
 
-url = f"https://drive.google.com/uc?export=download&id={upload_resume['id']}"
+def main():
+    drive = authenticate()
 
+    # Deleting the old Resume from my Drive
+    file_name = "Resume_Jaspreet_Singh_Dhami.pdf"  # Change this to your file name
+    deleting_file(file_name,drive)
+
+    # new_resume_path
+    resume_path = f"../Final_Job_Search/{file_name}"
+    resume = uploading_file(resume_path, file_name, drive)
+
+    url = f"https://drive.google.com/uc?export=download&id={resume['id']}"
+
+    print(f"paste this link in the button:\n{url}")
+
+main()
